d8edb3953108d0ec11a21b185d833c1e199524f7
fingoldin <vassilioskaxiras@gmail.com>
2016-12-08 23:26:34 -0500

    Made final page

--- jsPsych/plugins/jspsych-final.js
+++ jsPsych/plugins/jspsych-final.js
@@ -10,16 +10,51 @@ jsPsych.plugins["final"] = (function()
 
 		display_element.empty();
 
+
+		var wrap = document.createElement("DIV")
 		var top = document.createElement("DIV");
 		var bot = document.createElement("DIV");
 
-		display_element.append(top).append(bot);
+		$(wrap).append(top).append(bot);
+		display_element.append(wrap);
 
-		bot.style.fontSize = "55px";
-		top.style.fontSize = "35px";
+		function tfs(s) {
+			top.style.fontSize = s + "px";
+			top.style.lineHeight = s + "px";
+		}
+
+		function bfs(s) {
+                        bot.style.fontSize = s + "px";
+                        bot.style.lineHeight = s + "px";
+                }
+
+		tfs(55);
+		bfs(35);
+
+		top.style.opacity = bot.style.opacity = "0";
+
 
 		top.innerHTML = "Congratulations!";
 		bot.innerHTML = "The experiment is now over.";
+
+		$(top).fadeIn(400, function() {
+			$(bot).fadeIn(400);
+
+			setTimeout(2000, function() {
+				$(wrap).fadeOut(400, function() {
+					bot.innerHTML = "";
+					tfs(40);
+					bfs(50);
+					bot.style.opacity = "0";
+					bot.innerHTML = trial.points + " * 0.025 = $" + (trial.points * 0.025);
+					top.innerHTML = "You scored " + trial.points + (trial.points === 1 ? " point" : " points") + ", and receive");
+
+					$(wrap).fadeIn(400, function() {
+						bot.fadeIn(
+					});
+				});
+			});
+		});
 	}
 
 	return plugin;
